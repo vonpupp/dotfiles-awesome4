0f3be0a84d42fb3bfc57c6ac6ab53a1068ea8364
vonpupp <vonpupp@gmail.com>
2017-01-25 22:31:21 -0200

    Change terminal to ENV or urxvt

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal        = os.getenv("TERMINAL") or "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 

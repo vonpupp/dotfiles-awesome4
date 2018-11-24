b7ea4b31cbd7879da701c6fb0d70a4ce90b41a9e
vonpupp <albert@haevas.com>
2018-11-24 17:05:14 -0200

    Add eurkey layout

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -17,7 +17,7 @@ require("obvious.keymap_switch")
 require("obvious.temp_info")
 require("obvious.volume_alsa")
 
-obvious.keymap_switch.set_layouts({ "us", "us(intl)", "us(dvorak)", "br", "es", "il" })
+obvious.keymap_switch.set_layouts({ "eu", "us(dvorak)", "il", "us", "us(intl)", "br", "es" })
 
 sep  = "  |  "
 
